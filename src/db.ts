ea59a9ea4969939c74ff6694565578757c6c528d
C1letner <info@mrluckysgolf.com>
2026-09-23 20:36:39 -0700

    build 001: add supabase client

---
+++ src/db.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+import { config } from "./config.js";
+
+export const db = createClient(
+  config.supabaseUrl,
+  config.supabaseServiceRoleKey,
+  {
+    auth: { persistSession: false, autoRefreshToken: false }
+  }
+);
