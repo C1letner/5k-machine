dc1ae0b42c868eb9549d2ee70a88a5a44df81c01
C1letner <info@mrluckysgolf.com>
2026-09-24 07:56:53 -0700

    build 001: connect with Supabase secret key

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import { config } from "./config.js";
 
 export const db = createClient(
   config.supabaseUrl,
-  config.supabaseServiceRoleKey,
+  config.supabaseSecretKey,
   {
     auth: { persistSession: false, autoRefreshToken: false }
   }
