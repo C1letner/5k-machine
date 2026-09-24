982c9a5ae00b0503ee7ddb572e3e9142ff993be8
C1letner <info@mrluckysgolf.com>
2026-09-24 07:56:51 -0700

    build 001: use Supabase secret key

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ function required(name: string): string {
 
 export const config = {
   supabaseUrl: required("SUPABASE_URL"),
-  supabaseServiceRoleKey: required("SUPABASE_SERVICE_ROLE_KEY"),
+  supabaseSecretKey: required("SUPABASE_SECRET_KEY"),
   btcPriceUrl:
     process.env.BTC_PRICE_URL ??
     "https://api.coinbase.com/v2/prices/BTC-USD/spot",
